acd5e7f1373f3b82c1eeda40957ae5ecbc455983
xala3pa <salazar3pa@gmail.com>
2013-09-06 12:50:45 +0200

    function definitions starter

--- Beginning-Student-Language-Module/Learning-goals/function-definitions-starter.rkt
+++ Beginning-Student-Language-Module/Learning-goals/function-definitions-starter.rkt
@@ -5,6 +5,11 @@
 
 ;; function-definitions-starter.rkt
 
-(above (circle 40 "solid" "red")         
-       (circle 40 "solid" "yellow")
-       (circle 40 "solid" "green"))
+(define (bulb c)
+  (circle 40 "solid" c))
+
+(above (bulb "red")
+       (bulb "yellow")
+       (bulb "purple"))
+
+(bulb (string-append "re" "d"))
